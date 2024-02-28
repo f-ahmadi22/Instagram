386f53a77247619bb5dc8d1df357d6035647758c
CE9752 <fateme.ahmadi1522@gmail.com>
2024-02-28 19:59:07 +0330

    Add content urls.py

--- instagram/instagram/urls.py
+++ instagram/instagram/urls.py
@@ -20,4 +20,5 @@ from django.conf import settings
 
 urlpatterns = [
     path(settings.ADMIN_PATH, admin.site.urls),  # Custom admin path
+    path('content/', include('content.urls')),  # Content app urls
 ]
