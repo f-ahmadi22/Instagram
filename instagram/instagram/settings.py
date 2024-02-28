805d34f1370db3880da12d427834d0c6fdce4d2a
CE9752 <fateme.ahmadi1522@gmail.com>
2024-02-28 19:51:04 +0330

    Create app log

--- instagram/instagram/settings.py
+++ instagram/instagram/settings.py
@@ -43,7 +43,9 @@ INSTALLED_APPS = [
     'content.apps.ContentConfig',
     'user.apps.UserConfig',
     'user_activity.apps.UserActivityConfig',
+    'log.apps.LogConfig',
     
+
 ]
 
 MIDDLEWARE = [
