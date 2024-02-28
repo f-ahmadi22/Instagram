d850d0544f7ec0752aa9b0ee3c4dc724c14a61b3
CE9752 <fateme.ahmadi1522@gmail.com>
2024-02-28 21:24:29 +0330

    User app admin panel customization

--- instagram/user/admin.py
+++ instagram/user/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.admin import register
+from .models import User, UserRelationship
 
 # Register your models here.
+
+
+@register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'is_private', 'bio', 'created_at')
+    list_display_links = ('id', 'username', 'email', 'bio', 'is_private')
+    list_filter = ('is_private',)
+    list_editable = ('is_private',)
+    search_fields = ('username', 'bio')
+    ordering = ('id',)
+
+
+@register(UserRelationship)
+class UserRelationshipAdmin(admin.ModelAdmin):
+    list_display = ('id', 'follower', 'following', 'created_at')
+    list_display_links = ('id', 'follower', 'following')
+    search_fields = ('follower', 'following')
+    ordering = ('id',)
+
