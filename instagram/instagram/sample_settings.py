797decaac3965ff7c71ff1b168cf5d178009423c
CE9752 <fateme.ahmadi1522@gmail.com>
2024-02-28 19:24:10 +0330

    Create Instagram Django project

---
+++ instagram/instagram/sample_settings.py
@@ -0,0 +1,18 @@
+# Database settings
+DATABASE = {
+    'name': '',
+    'user': '',
+    'password': '',
+    'host': 'localhost',
+    'port': '5432',
+}
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = ''
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Custom Admin path
+ADMIN_PATH = 'your-custom-admin-path/'
