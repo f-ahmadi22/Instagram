6e5f2914151a56b90dac25160864c57cebd608c9
CE9752 <fateme.ahmadi1522@gmail.com>
2024-03-03 13:40:59 +0330

    Add view urls

--- instagram/content/urls.py
+++ instagram/content/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
 from rest_framework import routers
+from .views import CreatePostAPIView, FollowedUsersPostsAPIView, MentionUserAPIView
 
 
 urlpatterns = [
+    path('create-post/', CreatePostAPIView.as_view(), name='create-post'),
+    path('posts/', FollowedUsersPostsAPIView.as_view(), name='posts'),
+    path('mention/', MentionUserAPIView.as_view(), name='mention')
 ]
