3f45ea7528cb80dbab2725a7b79413bea1bbbe56
CE9752 <fateme.ahmadi1522@gmail.com>
2024-03-03 18:42:08 +0330

    Implment Like model

--- instagram/user_activity/models.py
+++ instagram/user_activity/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from user.models import MyUser
-from content.models import Post
+from content.models import Post, Story
 
 
 # Create your models here.
@@ -21,3 +21,30 @@ class Comment(models.Model):
 
     def likes(self):
         return self
+
+
+class LikePost(models.Model):
+    author = models.ForeignKey(MyUser, on_delete=models.CASCADE, related_name='like_post', verbose_name='author')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='like_post', verbose_name='post')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='created at')
+
+    def __str__(self):
+        return f'{self.author.username} liked post {self.post.id}'
+
+
+class LikeComment(models.Model):
+    author = models.ForeignKey(MyUser, on_delete=models.CASCADE, related_name='like_comment', verbose_name='author')
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name='like_comment', verbose_name='comment')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='created at')
+
+    def __str__(self):
+        return f'{self.author.username} liked comment {self.comment.id}'
+
+
+class LikeStory(models.Model):
+    author = models.ForeignKey(MyUser, on_delete=models.CASCADE, related_name='like_story', verbose_name='author')
+    story = models.ForeignKey(Story, on_delete=models.CASCADE, related_name='like_story', verbose_name='story')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='created at')
+
+    def __str__(self):
+        return f'{self.author.username} liked story {self.story.id}'
