7b2c57352775dab33f2f0429284026a08c570adb
CE9752 <fateme.ahmadi1522@gmail.com>
2024-03-03 13:25:12 +0330

    Create post apiview

--- instagram/content/views.py
+++ instagram/content/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Post, Story
+from .serializers import PostSerializer
 # Create your views here.
+
+
+class CreatePostAPIView(APIView):
+    def post(self, request):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
