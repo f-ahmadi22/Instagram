939cfbe5c97b6c050bca2d15a5c7f3744e5a76e8
CE9752 <fateme.ahmadi1522@gmail.com>
2024-03-03 18:25:11 +0330

    Implment Comment model

--- instagram/user_activity/models.py
+++ instagram/user_activity/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from user.models import MyUser
+from content.models import Post
+
 
 # Create your models here.
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(MyUser, on_delete=models.CASCADE, related_name='comments', verbose_name='author')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments', verbose_name='post')
+    text = models.TextField(verbose_name='text')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='created at')
+    is_reply = models.BooleanField(default=False, verbose_name='is reply')
+
+    def __str__(self):
+        return self.text
+
+    def replies(self):
+        return self
+
+    def likes(self):
+        return self
