26387dbaafa1749e579a011873252cc8f2ae5679
CE9752 <fateme.ahmadi1522@gmail.com>
2024-03-04 21:01:03 +0330

    Add user_activity urls

--- instagram/user_activity/urls.py
+++ instagram/user_activity/urls.py
@@ -1,4 +1,13 @@
+from rest_framework.routers import DefaultRouter
 from django.urls import path, include
+from . import views
+
+router = DefaultRouter()
+router.register(r'comments', views.CommentViewSet)
+router.register(r'like-posts', views.LikePostViewSet)
+router.register(r'like-comments', views.LikeCommentViewSet)
+router.register(r'like-stories', views.LikeStoryViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
 ]
