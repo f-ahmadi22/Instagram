80d23c819174d2ca65175516c64cd048f667c350
CE9752 <fateme.ahmadi1522@gmail.com>
2024-03-05 19:48:19 +0330

    Implement ProfileView model

--- instagram/log/models.py
+++ instagram/log/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from user.models import MyUser
 
 # Create your models here.
+
+
+class ProfileView(models.Model):
+    user = models.ForeignKey(MyUser, on_delete=models.CASCADE, related_name='profile_viewer', verbose_name='user')
+    user_profile = models.ForeignKey(MyUser, on_delete=models.CASCADE, related_name='profile_view',
+                                     verbose_name='user profile')
+    timestamp = models.DateTimeField(auto_now_add=True, verbose_name='timestamp')
+
+    def __str__(self):
+        return f'{self.user.username} viewed {self.user_profile.username}'
