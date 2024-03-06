cfd294ce5c28033eaa0190d161c3477ed044b2c0
CE9752 <fateme.ahmadi1522@gmail.com>
2024-03-06 09:26:23 +0330

    Log app commenting

--- instagram/log/admin.py
+++ instagram/log/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.admin import register
+from .models import ProfileView, PostView, StoryView
 
 # Register your models here.
+
+
+@register(PostView)
+class PostViewAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'post', 'timestamp')  # List of fields to display
+    list_display_links = ('id', 'user', 'post', 'timestamp')
+    list_filter = ('user', 'post')  # Filter fields
+    search_fields = ('user__username', 'post__caption')  # Search through these fields
+    ordering = ('-timestamp',)  # Order by creation time
+
+
+@register(StoryView)
+class StoryViewAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'story', 'timestamp')  # List of fields to display
+    list_display_links = ('id', 'user', 'story', 'timestamp')
+    list_filter = ('user', 'story')  # Filter fields
+    search_fields = ('user__username',)  # Search through these fields
+    ordering = ('-timestamp',)  # Order by creation time
+
+
+@register(ProfileView)
+class ProfileViewAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'user_profile', 'timestamp')  # List of fields to display
+    list_display_links = ('id', 'user', 'user_profile', 'timestamp')
+    list_filter = ('user', 'user_profile')  # Filter fields
+    search_fields = ('user__username', 'user_profile__username')  # Search through these fields
+    ordering = ('-timestamp',)  # Order by creation time
+
+
--- instagram/log/signals.py
+++ instagram/log/signals.py
@@ -8,25 +8,25 @@ post_viewed = Signal()
 
 
 @receiver(post_save, sender=ProfileView)
-def profile_view_handler(sender, instance, created, **kwargs):
+def profile_view_handler(sender, instance, created, **kwargs):  # Handle profile view signal received
 
     if created:
-        instance.user_profile.view_count += 1
-        instance.save()
+        instance.user_profile.view_count += 1  # Increase user's view_count by one
+        instance.save()  # Save instance
         post_viewed.send(sender=sender, instance=instance, user=instance.user)
 
 
 @receiver(post_save, sender=PostView)
-def post_view_handler(sender, instance, created, **kwargs):
+def post_view_handler(sender, instance, created, **kwargs):  # Handle post view signal received
     if created:
-        instance.post.view_count += 1
-        instance.save()
+        instance.post.view_count += 1  # Increase post's view_count by one
+        instance.save()  # Save instance
         post_viewed.send(sender=sender, instance=instance, user=instance.user)
 
 
 @receiver(post_save, sender=StoryView)
-def story_view_handler(sender, instance, created, **kwargs):
+def story_view_handler(sender, instance, created, **kwargs):  # Handle story view signal received
     if created:
-        instance.story.view_count += 1
-        instance.save()
+        instance.story.view_count += 1  # Increase story's view_count by one
+        instance.save()  # Save instance
         post_viewed.send(sender=sender, instance=instance, user=instance.user)
