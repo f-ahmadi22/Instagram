e5c7a9abfac4dbbed72aece4041125c1dfc37035
CE9752 <fateme.ahmadi1522@gmail.com>
2024-03-06 23:58:40 +0330

    Change user admin panel password

--- instagram/user/admin.py
+++ instagram/user/admin.py
@@ -1,28 +1,34 @@
 from django.contrib import admin
-from django.contrib.admin import register
+from django.contrib.auth.admin import UserAdmin
 from .models import MyUser, UserRelationship
 
-# Register your models here.
 
-
-@register(MyUser)
-class UserAdmin(admin.ModelAdmin):
+class MyUserAdmin(UserAdmin):
     model = MyUser
-    list_display = ('id', 'username', 'email', 'profile_picture', 'bio', 'date_joined', 'is_active', 'is_staff',
-                    'is_superuser', 'is_private')  # List of fields to display
     fieldsets = (
-        (None, {'fields': ('username', 'email', 'profile_picture', 'bio')}),
+        (None, {'fields': ('username', 'email', 'password', 'profile_picture', 'bio')}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'is_private')}),
-        ('Important dates', {'fields': ('last_login', 'date_joined')}),
-    )  # Define fieldset
-    readonly_fields = ('last_login', 'date_joined')  # Readonly fields
+        ('Important dates', {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'is_active', 'is_staff', 'is_superuser'),
+        }),
+    )
+    list_display = ('id', 'username', 'email', 'profile_picture', 'bio', 'date_joined', 'is_active', 'is_staff',
+                    'is_superuser', 'is_private', 'view_count')  # List of fields to display
+    search_fields = ('username', 'email')  # Search through these fields
     ordering = ('id',)  # Order by id
 
 
-@register(UserRelationship)
+admin.site.register(MyUser, MyUserAdmin)
+
+
+@admin.register(UserRelationship)
 class UserRelationshipAdmin(admin.ModelAdmin):
     list_display = ('id', 'follower', 'following', 'created_at')  # List of fields to display
     list_display_links = ('id', 'follower', 'following')
-    search_fields = ('follower', 'following')  # Search through these fields
+    search_fields = ('follower__username', 'following__username')  # Search through these fields
     ordering = ('id',)  # Order by id
 
